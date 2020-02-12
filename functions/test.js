172392cef2c2205908cdfe1974d414ed41ed0c90
abbas ashraf <amughal@digital-dividend.com>
2020-02-12 16:16:44 +0500

    test1

--- functions/test.js
+++ functions/test.js
@@ -1,5 +1,5 @@
 exports.handler = (event, context, callback) => {
-    console.log('queryStringParameters', event.queryStringParameters)
+    console.log('queryStringParameters', event, context)
     callback(null, {
         statusCode: 200,
         body: 'Test first function!',
